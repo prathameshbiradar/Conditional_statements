6a8333ae453ba481943433817b162a78d6268ef3
prathmeshbiradar <psbiradar948@gmail.com>
2024-10-08 15:24:45 +0530

    Changed Class Name and written a code in IDE.

---
+++ Conditional_Statements/If_else.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Statements
+{
+     class if_else
+    {
+        static void Main(string[] args)
+        {
+            int age = 20;
+            if (age > 18) {
+                Console.WriteLine("You can Vote");
+            }
+            else if (age > 22) 
+            {
+                Console.WriteLine("You can vote and also participate in election");
+            }
+            else
+            {
+                Console.WriteLine("you cannot vote");
+            }
+        }
+    }
+}
